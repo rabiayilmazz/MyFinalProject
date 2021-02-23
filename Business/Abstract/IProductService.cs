dcdd2e4629e2fc2fb6213bc50214cdd066e3022e
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-02-24 01:13:29 +0300

    10. hafta part 2

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,11 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllbyCategoryId(int id);
-        List<Product> GetByUnitePrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int productId);
-        IResult Add(Product product);
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllbyCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitePrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
+        Result Add(Product product);
     }
 }
